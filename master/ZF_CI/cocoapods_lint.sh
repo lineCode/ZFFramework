7ce9c9e4e4c6b93b23153fdbe01a22f5e4a8aa21
ZSaberLv0 <z@zsaber.com>
2018-03-19 12:45:02 +0800

    update CI

--- master/ZF_CI/cocoapods_lint.sh
+++ master/ZF_CI/cocoapods_lint.sh
@@ -9,10 +9,11 @@ _OLD_DIR=$(pwd)
 cd "$ZF_ROOT_PATH"
 if test "x-$POD_SUBSPEC" = "x-" ; then
     pod lib lint --allow-warnings --use-libraries --skip-import-validation --verbose
+    _RESULT="$?"
 else
     pod lib lint --allow-warnings --use-libraries --skip-import-validation --verbose --subspec=$POD_SUBSPEC
+    _RESULT="$?"
 fi
-_RESULT="$?"
 cd "$_OLD_DIR"
 
 exit $_RESULT
