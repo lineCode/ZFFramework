609cab802ecac5391c360bb0e9d7735616adad7d
ZSaberLv0 <z@zsaber.com>
2018-07-20 11:12:26 +0800

    optimize xUnique impl

--- tools/spec/iOS/unique_proj.sh
+++ tools/spec/iOS/unique_proj.sh
@@ -10,21 +10,33 @@ ZF_ROOT_PATH=$WORK_DIR/../../..
 ZF_TOOLS_PATH=$ZF_ROOT_PATH/tools
 
 sh "$ZF_TOOLS_PATH/common/zfsh.sh" "$WORK_DIR/zf3rd/zfautoscript_zf3rd_setup_xUnique.zfsh"
-rm -rf "$PROJ_PATH.zftmp" >/dev/null 2>&1
-cp -r "$PROJ_PATH" "$PROJ_PATH.zftmp" >/dev/null 2>&1
-python "$WORK_DIR/zf3rd/_repo/xUnique/xUnique.py" "$PROJ_PATH.zftmp"
 
-if test -e "$PROJ_PATH.zftmp/project.pbxproj.ubak" ; then
-    mv "$PROJ_PATH.zftmp/project.pbxproj.ubak" "$PROJ_PATH.zftmp/project.pbxproj"
+if test -d "$PROJ_PATH" ; then
+    _SRC_PATH="$PROJ_PATH/project.pbxproj"
+    _TMP_PATH="$PROJ_PATH/zfxunique_tmp"
+    _DST_PATH="$_TMP_PATH/project.pbxproj"
+else
+    _SRC_PATH="$PROJ_PATH"
+    _TMP_PATH="${PROJ_PATH%[/\\]*}/zfxunique_tmp"
+    _DST_PATH="$_TMP_PATH/project.pbxproj"
 fi
 
-if test -e "$PROJ_PATH.zftmp/project.pbxproj.sbak" ; then
-    mv "$PROJ_PATH.zftmp/project.pbxproj.sbak" "$PROJ_PATH.zftmp/project.pbxproj"
+rm -rf "$_TMP_PATH" >/dev/null 2>&1
+mkdir -p "$_TMP_PATH" >/dev/null 2>&1
+cp -r "$_SRC_PATH" "$_DST_PATH" >/dev/null 2>&1
+python "$WORK_DIR/zf3rd/_repo/xUnique/xUnique.py" "$_DST_PATH"
+
+if test -e "$_TMP_PATH/project.pbxproj.ubak" ; then
+    mv "$_TMP_PATH/project.pbxproj.ubak" "$_TMP_PATH/project.pbxproj"
 fi
 
-rsync -ruc "$PROJ_PATH.zftmp/project.pbxproj" "$PROJ_PATH/project.pbxproj" >/dev/null 2>&1
+if test -e "$_TMP_PATH/project.pbxproj.sbak" ; then
+    mv "$_TMP_PATH/project.pbxproj.sbak" "$_TMP_PATH/project.pbxproj"
+fi
 
-rm -rf "$PROJ_PATH.zftmp" >/dev/null 2>&1
+rsync -ruc "$_DST_PATH" "$_SRC_PATH" >/dev/null 2>&1
+
+rm -rf "$_TMP_PATH" >/dev/null 2>&1
 
 exit 0
 
