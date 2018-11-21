cc6c33cbcd5f6457a6c36e9ea27846f387d37b45
ZSaberLv0 <z@zsaber.com>
2018-11-21 15:08:29 +0800

    update CI

---
+++ master/ZF_CI/upload_app.sh
@@ -0,0 +1,42 @@
+WORK_DIR=$(cd "$(dirname "$0")"; pwd)
+APP_PATH=$1
+REMOTE_FILE=$2
+
+if test "x-$APP_PATH" = "x-" || test "x-$REMOTE_FILE" = "x-" ; then
+    echo "usage:"
+    echo "  upload_app.sh APP_PATH REMOTE_FILE"
+    exit 1
+fi
+
+if ! test -e "$APP_PATH" ; then
+    echo "file not exist: $APP_PATH"
+    exit 1
+fi
+
+ZF_ROOT_PATH=$WORK_DIR/../..
+ZF_TOOLS_PATH=$ZF_ROOT_PATH/tools
+REPO_PATH="$ZF_ROOT_PATH/_tmp/upload_app"
+REPO_PATH_TMP="$ZF_ROOT_PATH/_tmp/upload_app_tmp"
+
+sh "$ZF_TOOLS_PATH/common/git_check.sh" "https://github.com/ZFFramework/AppArchive" master "$REPO_PATH"
+cp "$APP_PATH" "$REPO_PATH/$REMOTE_FILE"
+
+_OLD_DIR=$(pwd)
+cd "$REPO_PATH"
+git config user.email "z@zsaber.com"
+git config user.name "ZSaberLv0"
+git config push.default "simple"
+git checkout .
+git reset --hard
+git clean -xdf
+git pull
+
+sh "$ZF_TOOLS_PATH/common/copy_check.sh" "$REPO_PATH" "$REPO_PATH_TMP"
+git filter-branch --force --index-filter 'git rm --cached --ignore-unmatch *' --prune-empty --tag-name-filter cat -- --all
+sh "$ZF_TOOLS_PATH/common/copy_check.sh" "$REPO_PATH_TMP" "$REPO_PATH"
+
+git add -A
+git commit -a -m "update by CI"
+git push --force "https://ZSaberLv0:$ZFCI_DOC_TOKEN@github.com/ZFFramework/AppArchive"
+cd "$_OLD_DIR"
+
